64dc9ba117d817baab68386af1f49aa5f757b054
Charly <icharlieanne@gmail.com>
2023-09-10 00:32:53 +0100

    fix

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 function updateCurrentTime() {
-<<<<<<< HEAD
   const currentUTCTimeElement = document.getElementById('current-utc-time');
 
   const now = new Date();
-  const options = {
-    timeZone: 'Africa/Lagos', // (WAT)
-  };
-  const watTime = now.toLocaleTimeString('en-US', options);
+
+  const watTime = now.toLocaleTimeString('en-US');
 
   currentUTCTimeElement.textContent = `${watTime}`;
 }
@@ -30,29 +27,3 @@ function updateCurrentDay() {
   currentDayElement.textContent = `Today is ${dayOfWeek}`;
 }
 updateCurrentDay();
-=======
-    const currentUTCTimeElement = document.getElementById('current-utc-time');
-
-    const now = new Date();
-    const options = {
-        timeZone: 'Africa/Lagos', // (WAT)
-        hour12: false, // 24-hour format
-    };
-    const watTime = now.toLocaleTimeString('en-US', options);
-
-    currentUTCTimeElement.textContent = `${watTime}`;
-}
-updateCurrentTime();
-
-function updateCurrentDay() {
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const currentDayElement = document.getElementById('current-day');
-    
-    const today = new Date();
-    const dayOfWeek = daysOfWeek[today.getDay()];
-    
-    currentDayElement.textContent = `Today is ${dayOfWeek}`;
-}
-updateCurrentDay();
-
->>>>>>> ec1dd5747dccd19c4e9411a0c395d5856054e175
