0208d64819996eae0386566501781ea2942c195c
Charly <icharlieanne@gmail.com>
2023-09-10 00:28:23 +0100

    time logic

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-function updateUTCTime() {
-  const utcTimeElement = document.getElementById('utc-time');
-  const currentUTCTime = new Date().toUTCString();
-  utcTimeElement.textContent = currentUTCTime;
-}
-updateUTCTime();
+function updateCurrentTime() {
+  const currentUTCTimeElement = document.getElementById('current-utc-time');
 
-// UTC time updated every second (for real-time updating)
-setInterval(updateUTCTime, 1000);
+  const now = new Date();
+  const options = {
+    timeZone: 'Africa/Lagos', // (WAT)
+  };
+  const watTime = now.toLocaleTimeString('en-US', options);
+
+  currentUTCTimeElement.textContent = `${watTime}`;
+}
+updateCurrentTime();
+
+function updateCurrentDay() {
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const currentDayElement = document.getElementById('current-day');
+
+  const today = new Date();
+  const dayOfWeek = daysOfWeek[today.getDay()];
+
+  currentDayElement.textContent = `Today is ${dayOfWeek}`;
+}
+updateCurrentDay();
